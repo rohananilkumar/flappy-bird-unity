fa37dfeda048a75da5670ff4a84c01af787cf6fa
rohananilkumar <ro.anilpnr@gmail.com>
2022-07-02 11:45:36 +0530

    Bug fixes

--- Assets/Player/PlayerMover.cs
+++ Assets/Player/PlayerMover.cs
@@ -55,6 +55,7 @@ public class PlayerMover : MonoBehaviour
         {
             scoreKeeper.Increment();
             Debug.Log("Score increment "+scoreKeeper.Score.ToString());
+            Destroy(other);
         }
     }
 }
